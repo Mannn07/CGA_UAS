b5958c51af9f8bbea73cae2a0774d836c8a3f747
Mannn07 <127299958+Mannn07@users.noreply.github.com>
2024-09-25 14:24:35 +0700

    Update ProjectSetup.cs

--- Assets/ProjectSetup.cs
+++ Assets/ProjectSetup.cs
@@ -1,61 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using System.IO;
 
-public class ProjectSetup
+public class NewBehaviourScript : MonoBehaviour
 {
-    [MenuItem("Tools/Create Project Structure")]
-    public static void CreateProjectStructure()
-    {
-        string rootPath = Application.dataPath;
-  //comment
-        // Create main folders
-        string[] folders = new string[]
-        {
-            "Animations",
-            "Audio",
-            "Materials",
-            "Prefabs",
-            "Scenes",
-            "Scripts",
-            "UI"
-        };
-
-        foreach (string folder in folders)
-        {
-            string path = Path.Combine(rootPath, folder);
-            if (!AssetDatabase.IsValidFolder(path))
-            {
-                AssetDatabase.CreateFolder("Assets", folder);
-            }
-        }
-
-        // Create a sample script in Scripts folder
-        string scriptPath = Path.Combine(rootPath, "Scripts", "SampleScript.cs");
-        if (!File.Exists(scriptPath))
-        {
-            File.WriteAllText(scriptPath, 
-@"using UnityEngine;
-
-public class SampleScript : MonoBehaviour
-{
+    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(""Hello, World!"");
+        
     }
-}");
-            AssetDatabase.Refresh();
-        }
 
-        // Create a sample scene in Scenes folder
-        string scenePath = Path.Combine(rootPath, "Scenes", "MainScene.unity");
-        if (!File.Exists(scenePath))
-        {
-            EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
-            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), scenePath);
-        }
-
-        AssetDatabase.Refresh();
-        Debug.Log("Project structure created successfully!");
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
